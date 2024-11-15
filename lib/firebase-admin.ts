8b4976e8cb95d611c59d12673d9c0a8959997a02
Vinayak Joshi <vinayakjoshi1930@gmail.com>
2024-11-15 09:37:36 +0530

    bug fixed

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,21 +1,18 @@
-
-import { getApps, initializeApp, cert, App } from 'firebase-admin/app';
-import { getAuth } from 'firebase-admin/auth';
+import { initializeApp, getApps, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
-
-let adminApp: App;
+import { getAuth } from 'firebase-admin/auth';
 
 if (!getApps().length) {
-  adminApp = initializeApp({
+  const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
+  
+  initializeApp({
     credential: cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      privateKey: privateKey,
     }),
   });
 }
 
-const adminAuth = getAuth();
-const adminDb = getFirestore();
-
-export { adminAuth, adminDb };
+export const adminDb = getFirestore();
+export const adminAuth = getAuth();
